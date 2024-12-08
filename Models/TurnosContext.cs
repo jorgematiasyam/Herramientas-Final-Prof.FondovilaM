f4c7bd80905ea56d64d9033ab4b92443afb6425e
jorgematiasyam <jorgematiasyam@gmail.com>
2024-12-07 21:06:56 -0300

    Configuro connectionSettings, agrego controlador Especialidad

---
+++ Models/TurnosContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+namespace Turnos.Models
+{
+    public class TurnosContext : DbContext
+    {
+        public TurnosContext(DbContextOptions<TurnosContext> opciones)
+        : base(opciones)
+        {
+
+        }
+
+        public DbSet<Especialidad> Especialidad {get; set; }
+
+
+    }
+
+}

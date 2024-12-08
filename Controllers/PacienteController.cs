8cd9d5ecc6ddb31e6702b2b3062b95e98b17df48
jorgematiasyam <jorgematiasyam@gmail.com>
2024-12-08 01:07:09 -0300

    Agrego metodo Create en Paciente cOntroller

--- Controllers/PacienteController.cs
+++ Controllers/PacienteController.cs
@@ -19,11 +19,45 @@ namespace Turnos.Controllers
             return View(await _context.Paciente.ToListAsync());
         }
 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
 
+            var paciente = await _context.Paciente.FirstOrDefaultAsync(p => p.IdPaciente == id);
+            if(paciente == null)
+            {
+                return NotFound();
+            }
+            return View(paciente);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPaciente, Nombre, Apellido, Direccion, Telefono, Email")]Paciente paciente)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(paciente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(paciente);
+
+
+
+
+        }
 
     }
-
-
 
 }
 

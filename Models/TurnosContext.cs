2cbf016ceab07fc222488230bd39659a2d3e8fb0
jorgematiasyam <jorgematiasyam@gmail.com>
2024-12-08 00:31:32 -0300

    Metodo onModelCreating en Turnos context

--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -6,11 +6,23 @@ namespace Turnos.Models
         public TurnosContext(DbContextOptions<TurnosContext> opciones)
         : base(opciones)
         {
-
+            
         }
 
         public DbSet<Especialidad> Especialidad {get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Especialidad>(entidad => {
+                entidad.ToTable("Especialidad");
+
+                entidad.HasKey(e => e.IdEspecialidad);
+                entidad.Property(e => e.Descripcion)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+        }
 
     }
 
